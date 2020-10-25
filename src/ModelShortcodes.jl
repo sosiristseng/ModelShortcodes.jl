a95ae085534ac9f5ca6b034921cc40eb6f7cb814
Wen Wei Tseng <sosiristseng@gmail.com>
2020-10-25 17:43:40 +0800

    fix using unpack

--- src/ModelShortcodes.jl
+++ src/ModelShortcodes.jl
@@ -1,7 +1,5 @@
 module ModelShortcodes
 
-using UnPack
-
 export T0, F, R, VT, iVT, KEQ_MGATP, power_to_conc, conc_to_power, p_one, one_m, mm, mmr, hill, hillr, expit, nernst, pmf, ghk, sqrt_s, pow_s, exprel, stimulate, hill_s, hillr_s, eN, Δp, ieN
 
 include("units.jl")
